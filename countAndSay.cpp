2aca5d0a1a013db19b990a9ff89994a6fb5f1130
orientez <armstrongzhang1989@gmail.com>
2015-02-06 15:42:30 -0600

    Update countAndSay.cpp

--- countAndSay.cpp
+++ countAndSay.cpp
@@ -1,35 +1,48 @@
-    string strTostr(string str)
+#include <string>
+#include <iostream>
+using namespace std;
+string strTostr(string str)
+{
+    int cnt = 1;
+    string result;
+
+    for(int i = 0; i < str.length() -1 ; i++)
     {
-        int cnt = 1;
-        string result;
-        for(int i = 0; i < str.length() -1 ; i++)
+        if (str[i]==str[i+1]) 
         {
-            if (str[i]==str[i+1]) 
-            {
-                cnt++;
-            }
-            else
-            {
-                result.append(1, (char)cnt );
-                result.append(1, str[i]);
-                cnt = 1;
-            }
+            cnt++;
         }
-        
-        result.append(1, (char)cnt );
-        result.append(1, str[str.length() -1]);
-        return result;
+        else
+        {
+
+            result=result + to_string(cnt);
+            result=result + str[i];
+            cnt = 1;
+        }
     }
     
-    string countAndSay(int n) {
-        string str;
-        if (n==0) return str;
-        
-        str = "1";
-        //if (n==1) return str;
-        for (int i = 1; i <= n; i++)
-        {
-            str = strTostr(str);
-        }
-        return str;
+    result=result + to_string(cnt);
+    result=result + str[str.length() -1];
+    return result;
+}
+
+string countAndSay(int n) {
+    string str;
+    if (n==0) return str;
+    
+    str = "1";
+    //if (n==1) return str;
+    for (int i = 1; i <= n; i++)
+    {
+    	
+        str = strTostr(str);
     }
+    return str;
+}
+
+int main(int argc, char** argv)
+{
+	//string str("11");
+	cout << countAndSay(atoi(argv[1]))<< "\n";
+	return 0;
+}
