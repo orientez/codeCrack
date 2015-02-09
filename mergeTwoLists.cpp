6deaf80150278eff09a5c67214427f71086cbcc2
orientez <armstrongzhang1989@gmail.com>
2015-02-09 11:43:37 -0600

    Create mergeTwoLists.cpp

---
+++ mergeTwoLists.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        ListNode head(0);
+        
+        ListNode *mList;
+        head.next = mList;
+        int v;
+        while (l1 && l2)
+        {
+            
+            if (l1->val <= l2->val) 
+            {
+                v = l1->val;
+                l1 = l1->next;
+            }
+            else
+            {
+                v = l2->val;
+                l2 = l2->next;
+            }
+            
+            ListNode* n = new ListNode(v);
+            mList->next = n;
+            mList = mList->next;
+        }
+        
+        if (l1) mList->next = l1; 
+        if (l2) mList->next = l2;
+        if (!mList) return NULL;
+        
+        return head.next->next;
+    }
+};
