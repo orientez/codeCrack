ea7694d5efdebd46e50ac9b59b986425c197bade
orientez <armstrongzhang1989@gmail.com>
2015-02-09 10:47:43 -0600

    Update lengthOfLastWord.cpp

--- lengthOfLastWord.cpp
+++ lengthOfLastWord.cpp
@@ -1,13 +1,21 @@
-class Solution {
-public:
-    int lengthOfLastWord(const char *s) {
-        int si = 0;
-        int i = 0;
-        while(s[i])
-        {
-            i++;
-            if (s[i]==' ') si = i;
-        }
-        return (i-si);
+int lengthOfLastWord(const char *s) {
+    int si = 0;
+    int i = 0;
+    while(s[i])
+    {
+        i++;
+        // if (s[i]==' ') std::cout << i << "\n";
     }
-};
+    i--;
+    while(s[i]==' ')
+    {
+    	i--;
+    }
+    
+    while(s[i]!=' ')
+    {
+    	si++;
+    	i--;
+    }
+    return si;
+}
