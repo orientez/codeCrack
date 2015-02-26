c70729b635ba3bbfe06a9c02cf125a1af7d98fe2
orientez <armstrongzhang1989@gmail.com>
2015-02-26 10:23:13 -0600

    Create rotateArray.cpp

---
+++ rotateArray.cpp
@@ -0,0 +1,18 @@
+void rotate(int nums[], int n, int k) {
+    
+    k = k % n;
+    int *rotate = new int[k];
+    
+    for (int i = n - 1 ; i > n - k - 1; i--)
+    {
+        rotate[i - (n - k)] = nums[i];
+        nums[i] = nums[i - k];
+    }
+    
+    for (int i = 0; i < k; i++)
+    {
+        nums[i] = rotate[i];
+    }
+
+    delete rotate;
+}
