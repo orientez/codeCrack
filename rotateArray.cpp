e1886647c54846d9e6b298dc661c0223c6590386
orientez <armstrongzhang1989@gmail.com>
2015-02-26 13:02:55 -0600

    Update rotateArray.cpp

--- rotateArray.cpp
+++ rotateArray.cpp
@@ -1,6 +1,6 @@
 void rotate(int nums[], int n, int k) {
-    
     k = k % n;
+    //std::cout << k << "\n";
     int *rotate = new int[k];
     
     for (int i = n - 1 ; i > n - k - 1; i--)
@@ -8,7 +8,10 @@ void rotate(int nums[], int n, int k) {
         rotate[i - (n - k)] = nums[i];
         nums[i] = nums[i - k];
     }
-    
+    for (int i = n - k; i > k - 1; i--)
+    {
+    	nums[i] = nums[i - k];
+    }
     for (int i = 0; i < k; i++)
     {
         nums[i] = rotate[i];
