105aa1371c676720e74476f2f430438a8034f716
orientez <armstrongzhang1989@gmail.com>
2015-03-08 11:40:03 -0500

    Update findSubstring.cpp

--- findSubstring.cpp
+++ findSubstring.cpp
@@ -20,60 +20,68 @@ vector<int> findSubstring(string S, vector<string> &L) {
     int cnt = 0;
     bool reset = false;
     int start;
-    while (n+len-1 < S.length())
-    {
-        string sstr = S.substr(n, len);
-        while (map.find(sstr)==map.end() && (n+len-1) < S.length())
-        {
-            n++;
-            sstr = S.substr(n, len);
-            reset = true;
-        }
+	int left = 0;
+		while (n+len-1 < S.length())
+		{
+			//cout << left << "\n";
+		    string sstr = S.substr(n, len);
 
-        if ( (n+len-1) < S.length() )
-        {
-            if ( reset ) {resetMap(map, L); cnt=0; reset=false;}
-            map[sstr]=map[sstr]-1;
-            if (map[sstr]==0)
-            {
-                cnt++;
-                if (cnt==vs) 
-                {
-                    start = n-vs*len+len;
-                    v.push_back(start); 
-                    n = start + 1;
-                    
-                    resetMap(map, L); 
-                    cnt=0;
-                    
-                }
-            }
-            else if (map[sstr]<0)
-            {
-                int temp = n - len;
-                resetMap(map, L);
-                map[sstr]=map[sstr]-1;
-                cnt = 1;
-                
-                string str= S.substr(temp, len);
-                while (str!=sstr)
-                {
-                    map[str]=map[str]-1;
-                    temp = temp - len;
-                    str= S.substr(temp, len);
-                    cnt++;
-                }
-                n = n + len;
-                
-                
-            }
-            else
-            {
-                n = n + len;
-                cnt++;
-            }
-            
-        }
+		    while (map.find(sstr)==map.end() && (n+len-1) < S.length())
+		    {
+		    	left++;
+		    	n = left;
+		        //n++;
+		        sstr = S.substr(n, len);
+		        reset = true;
+		    }
+
+		    if ( (n+len-1) < S.length() )
+		    {
+		        if ( reset ) {resetMap(map, L); cnt=0; reset=false;}
+		        map[sstr]=map[sstr]-1;
+		        if (map[sstr]==0)
+		        {
+		        	n = n + len;
+		            cnt++;
+		            if (cnt==vs) 
+		            {
+		                start = n-vs*len;
+		                
+		                v.push_back(start); 
+		                if (left <= start + 1)
+		                	left = start + 1;
+		                else
+		                	left++;
+		                n = left;
+		                
+		                resetMap(map, L); 
+		                cnt=0;
+		            }
+		        }
+		        else if (map[sstr]<0)
+		        {
+		            int temp = n - len;
+		            resetMap(map, L);
+		            map[sstr]=map[sstr]-1;
+		            cnt = 1;
+		            
+		            string str= S.substr(temp, len);
+		            // modify this part
+		            while (str!=sstr)
+		            {
+		                map[str]=map[str]-1;
+		                temp = temp - len;
+		                str= S.substr(temp, len);
+		                cnt++;
+		            }
+		            n = n + len;
+		        }
+		        else
+		        {
+		            n = n + len;
+		            cnt++;
+		        }
+		    }
 
     }
     return v;
