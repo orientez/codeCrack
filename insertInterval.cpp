b48652d8e1817e8eee3bee1ae61aecf3e4b7248d
orientez <armstrongzhang1989@gmail.com>
2015-04-13 15:10:42 -0500

    Update insertInterval.cpp

--- insertInterval.cpp
+++ insertInterval.cpp
@@ -1,12 +1,21 @@
-/**
- * Definition for an interval.
- * struct Interval {
- *     int start;
- *     int end;
- *     Interval() : start(0), end(0) {}
- *     Interval(int s, int e) : start(s), end(e) {}
- * };
- */
+struct Interval {
+    int start;
+    int end;
+    Interval() : start(0), end(0) {}
+    Interval(int s, int e) : start(s), end(e) {}
+};
+
+
+
+void printInter(vector<Interval> is)
+{
+    for (int i = 0; i < is.size(); i++)
+    {
+        cout << "[" << is[i].start << ", " << is[i].end << "], ";
+    }
+    cout << "\n";
+
+}
 vector<Interval> insert(vector<Interval> &intervals, Interval newInterval) {
     int start = 0;
     int end = 0;
@@ -42,10 +51,10 @@ vector<Interval> insert(vector<Interval> &intervals, Interval newInterval) {
     end = (start/2)*2;
     while ( i < isize && !isEndFound)
     {
-        if (newInterval.end > intervals[i].start)
+        if (newInterval.end >= intervals[i].start)
         {
             end++;
-            if (newInterval.start > intervals[i].end )
+            if (newInterval.end > intervals[i].end )
             {
                 end++;
             }
@@ -56,14 +65,16 @@ vector<Interval> insert(vector<Interval> &intervals, Interval newInterval) {
             isEndFound = true;
         i++;
     }
+    cout << start << " " << end << "\n";
     ret.insert(ret.end(), intervals.begin(), intervals.begin()+start/2);
     Interval inter;
     if (start%2==0) inter.start = newInterval.start;
     else inter.start = intervals[start/2].start;
     
     if (end%2==0) inter.end = newInterval.end;
-    else inter.end = intervals[start/2].end;
+    else inter.end = intervals[end/2].end;
     ret.push_back(inter);
-    ret.insert(ret.end(), intervals.begin()+end/2+1, intervals.end());
+    ret.insert(ret.end(), intervals.begin()+(end+1)/2, intervals.end());
+    //printInter(ret);
     return ret;
 }
