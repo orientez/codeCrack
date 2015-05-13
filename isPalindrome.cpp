580600ed46be8a47232bf6de2dcfe0cda584a5f3
orientez <armstrongzhang1989@gmail.com>
2015-05-12 21:14:33 -0500

    Update isPalindrome.cpp

--- isPalindrome.cpp
+++ isPalindrome.cpp
@@ -1,31 +1,21 @@
-    bool isPalindrome(int x) {
-        if (x < 0) return false;
-        if (x < 10) return true;
-        
-        int base = 10;
-        
-        int nd = 0;
-        int cpx = x;
-        while (cpx > 0)
-        {
-            nd++;
-            cpx = cpx / base;
-        }
-        
-        int maxBase = static_cast<int>(pow(10, nd-1));
-        int h, l;
-        
-        for (int i = 0; i <=(nd-1)/2; i++)
-        {
-            h = x / maxBase;
-            l = x % base;
-            if (x % maxBase==0 && i!=0) return true;
-            
-            if ((h*base/10)!=l) return false;
-            x = x - h * maxBase - l * (base / 10);
-            
-            maxBase = maxBase / 10;
-            base = base * 10;
-        }
-        return true;
+bool isPalindrome(int x) {
+    if (x < 0) return false;
+    if (x < 10) return true;
+    int base = 1;
+    while (x/10 >= base) {
+        base = base * 10;
     }
+    //base = base/10;
+    while (x > 0) {
+        if (x < 10) return true;
+        if (x/base==x%10)
+        {
+            x = x%base;
+            x = x/10;
+            base = base/100;
+        }
+        else
+            return false;
+        
+    }
+    return true;
