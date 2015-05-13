14df689a71392b2d0ce44640cba14ad8c8b2948a
orientez <armstrongzhang1989@gmail.com>
2015-05-12 23:02:13 -0500

    Create minSubArrayLen.cpp

---
+++ minSubArrayLen.cpp
@@ -0,0 +1,26 @@
+int minSubArrayLen(int s, vector<int>& nums) {
+    int left = 0;
+    int right = 0;
+    int nn = nums.size();
+    if (nn==0) return 0;
+    int cur = nums[0];
+    int m;
+    int cnt = 1;
+    while (right<nn && left<=right) {
+        if (cur < s) {
+            right++;
+            cur = cur + nums[right];
+            cnt++;
+            m = cnt;
+        }
+        else {
+            while (cur >= s) {
+                cur = cur - nums[left];
+                left++;
+                cnt--;
+            }
+            m = min(m, cnt+1);
+        }
+    }
+    if (left==0) return 0;
+    else return m;
