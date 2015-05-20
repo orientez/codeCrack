66ade0425cba24a43b41d33dd80c9b33fca7cff9
orientez <armstrongzhang1989@gmail.com>
2015-05-20 10:16:04 -0500

    Update numIslands.cpp

--- numIslands.cpp
+++ numIslands.cpp
@@ -1,78 +1,44 @@
-bool isValid(int x, int y, int nrows, int ncols)
-{
-    return (x >= 0 && y >= 0 && x < nrows && y < ncols);
+bool isValid(int x, int y, int mx, int my) {
+    return (x >= 0 && x < mx && y >= 0 &&  y < my);
 }
 
-void bfs(vector<vector<char> > &grid, vector<vector<bool> >& isVisited, int i, int j)
-{
-    pair<int, int> pos(i, j);
-    queue<pair<int, int>> iqueue;
-    iqueue.push(pos);
-
-    while (!iqueue.empty())
-    {
-        pair<int, int> pos = iqueue.front();
-        iqueue.pop();
-        int x = pos.first;
-        int y = pos.second;
-        int nrows = grid.size();
-        int ncols = grid[0].size();
+void bfs(const vector<vector<char>>& grid, vector<vector<bool> >& isVisited, int x, int y) {
+    queue<pair<int, int> > coor;
+    coor.push(make_pair(x, y));
+    int mx = grid.size();
+    int my = grid[0].size();
+    while (!coor.empty()) {
+        pair<int, int> p = coor.front();
+        coor.pop();
+        int next[4][2];
+        next[0][0] = p.first+1;next[0][1] = p.second;
+        next[1][0] = p.first-1;next[1][1] = p.second;
+        next[2][0] = p.first;  next[2][1] = p.second+1;
+        next[3][0] = p.first;  next[3][1] = p.second-1;
         
-        if (isValid(x+1, y, nrows, ncols))
-        {
-            if (!isVisited[x+1][y])
-            {
-                iqueue.push(make_pair(x+1, y));
-                isVisited[x+1][y] = true;
-            }
-        }   
-        if (isValid(x-1, y, nrows, ncols))
-        {
-            if (!isVisited[x-1][y])
-            {
-                iqueue.push(make_pair(x-1, y));
-                isVisited[x-1][y] = true;
-            }    
-        }
-        if (isValid(x, y+1, nrows, ncols))
-        {
-            if (!isVisited[x][y+1])
-            {
-                iqueue.push(make_pair(x, y+1));
-                isVisited[x][y+1] = true;
-            }
-        }    
-        if (isValid(x, y-1, nrows, ncols))
-        {
-            if (!isVisited[x][y-1])
-            {
-                iqueue.push(make_pair(x, y-1));
-                isVisited[x][y-1] = true;
+        for (int i = 0; i < 4; i++) {
+            if ( isValid(next[i][0], next[i][1], mx, my ) ) {
+                isVisited[next[i][0]][next[i][1]] = true;
+                coor.push(make_pair(next[i][0], next[i][1]));
             }
         }
     }
-    
 }
 
-int numIslands(vector<vector<char>> &grid) {
-    int nrows = grid.size();
-    if (nrows==0) return 0;
-    int cols = grid[0].size();
-    vector<bool> vb(cols, false);
-    vector<vector<bool> > isVisited(nrows, vb);
-    int nIsland=0;
-    for (int i = 0; i < nrows; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
-            if (!isVisited[i][j] && grid[i][j]==1)
-            {
+int numIslands(vector<vector<char>>& grid) {
+    int nr = grid.size();
+    if (nr==0) return 0;
+    int nc = grid[0].size();
+    vector<vector<bool> > isVisited(nr, vector<bool>(nc, false));
+    int nIslands = 0;
+    for (int i = 0; i < nr; i++) {
+        for (int j = 0; j < nc; j++) {
+            if (!isVisited[i][j] && grid[i][j]=='1') {
+                nIslands++;
                 isVisited[i][j] = true;
                 bfs(grid, isVisited, i, j);
-                nIsland++;
             }
         }
     }
-    return nIsland;
-    
+    return nIslands;
 }
