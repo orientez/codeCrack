619bbc15505cf5b56a66bceb30ab5b338065b662
orientez <armstrongzhang1989@gmail.com>
2016-01-11 21:42:55 -0800

    remove empty lines

--- buildTree.cpp
+++ buildTree.cpp
@@ -1,16 +1,12 @@
 TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder) {
         if (postorder.size()==0) return NULL;
-        
         TreeNode *root = new TreeNode(postorder[postorder.size()-1]);
-        
         if (postorder.size()==1)  return root;
-
         unordered_map<int, int> mmap;
         for (int k = 0; k < inorder.size(); k++)
         {
             mmap[inorder[k]] = k;
         }
-        
         TreeNode *cur = NULL;
         bool isLocated = false;
         for (int i = postorder.size()-2; i >= 0; i--)
